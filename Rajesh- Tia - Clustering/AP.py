6e3c68a7fbcdebd4dafad26456a14523549cb39c
Rajesh Basrur <basrur.rajesh@gmail.com>
2016-07-18 17:44:01 -0400

    Update AP.py

--- Rajesh- Tia - Clustering/AP.py
+++ Rajesh- Tia - Clustering/AP.py
@@ -6,10 +6,6 @@ import numpy as np
 
 # Generate sample data
 centers = [[1, 1], [-1, -1], [1, -1]]
-#X, labels_true = make_blobs(n_samples=300, centers=centers, cluster_std=0.5,
-  #                          random_state=0)
-
-  # read_data_from_file
 
 file_name = "result.csv"
 
@@ -40,11 +36,6 @@ for ele in student_list:
   Matrix[i][1] = ele.Correct_Percentage
   i += 1
 
-# Matrix,labels_true
-    
-
-##############################################################################
-# Compute Affinity Propagation
 af = AffinityPropagation(preference=-0.25,affinity="euclidean").fit(Matrix)
 cluster_centers_indices = af.cluster_centers_indices_
 labels = af.labels_
@@ -53,18 +44,7 @@ print (labels)
 n_clusters_ = len(cluster_centers_indices)
 
 print('Estimated number of clusters: %d' % n_clusters_)
-#print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-#print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-#print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-#print("Adjusted Rand Index: %0.3f"
-#      % metrics.adjusted_rand_score(labels_true, labels))
-      #print("Adjusted Mutual Information: %0.3f"
-#      % metrics.adjusted_mutual_info_score(labels_true, labels))
-#print("Silhouette Coefficient: %0.3f"
-#      % metrics.silhouette_score(X, labels, metric='sqeuclidean'))
 
-##############################################################################
-# Plot result
 import matplotlib.pyplot as plt
 from itertools import cycle
 
